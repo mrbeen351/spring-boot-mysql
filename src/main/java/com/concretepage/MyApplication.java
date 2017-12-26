1ef4269816c67ebbf508ae4261877ebbd9664528
Thanh Nguyen <thanhcongnguyen@kms-technology.com>
2017-12-26 11:20:20 +0700

    First commit

---
+++ src/main/java/com/concretepage/MyApplication.java
@@ -0,0 +1,10 @@
+package com.concretepage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer {  
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+    }       
+}            
